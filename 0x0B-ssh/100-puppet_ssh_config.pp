71822631a5c733cebef62b83aa565fe355ac2caf
medayoubadri <medayoubadri@gmail.com>
2025-01-11 10:27:16 +0100

    Task 4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Puppet manifest to configure ssh client
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  match  => '^#?[[:space:]]*PasswordAuthentication',
+    path  => '/etc/ssh/ssh_config',
+    line  => '    PasswordAuthentication no',
+    match => '^#?[[:space:]]*PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
-  match  => '^#?[[:space:]]*IdentityFile',
+    path  => '/etc/ssh/ssh_config',
+    line  => '    IdentityFile ~/.ssh/school',
+    match => '^#?[[:space:]]*IdentityFile',
 }
