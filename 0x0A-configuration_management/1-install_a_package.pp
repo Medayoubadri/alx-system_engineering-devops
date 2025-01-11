7cbdb8863312931092806d697f650a60705a96cb
medayoubadri <medayoubadri@gmail.com>
2025-01-11 08:25:06 +0100

    Task-1 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 and Werkzeug 2.1.1 using pip3
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
