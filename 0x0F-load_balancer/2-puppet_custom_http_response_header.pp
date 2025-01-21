6234a970151cc8466ed907e3ac34e9fbf49c2284
medayoubadri <medayoubadri@gmail.com>
2025-01-21 15:09:46 +0100

    Task 2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# Puppet manifest to configure Nginx with a custom HTTP header
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    add_header X-Served-By ${hostname};
+    location / {
+        try_files ${uri} ${uri}/ =404;
+    }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
