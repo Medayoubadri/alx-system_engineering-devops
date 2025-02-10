355066db7c36efe09422203092374ccb14c5d375
medayoubadri <medayoubadri@gmail.com>
2025-02-10 16:29:11 +0100

    0x15. API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,35 +9,29 @@ if __name__ == "__main__":
     users_url = f"{base_url}/users"
     todos_url = f"{base_url}/todos"
 
-    try:
-        users_response = requests.get(users_url)
-        users_data = users_response.json()
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
+    users_response = requests.get(users_url)
+    users_data = users_response.json()
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-        all_employee_tasks = {}
+    all_employee_tasks = {}
 
-        for user in users_data:
-            user_id = str(user['id'])
-            username = user['username']
-            user_todos = [todo for todo in todos_data
-                          if todo['userId'] == user['id']]
+    for user in users_data:
+        user_id = str(user['id'])
+        username = user['username']
+        user_todos = [todo for todo in todos_data
+                      if todo['userId'] == user['id']]
 
-            tasks = []
-            for todo in user_todos:
-                tasks.append({
-                    "username": username,
-                    "task": todo['title'],
-                    "completed": todo['completed']
-                })
+        tasks = []
+        for todo in user_todos:
+            tasks.append({
+                "username": username,
+                "task": todo['title'],
+                "completed": todo['completed']
+            })
 
-            all_employee_tasks[user_id] = tasks
+        all_employee_tasks[user_id] = tasks
 
-        filename = "todo_all_employees.json"
-        with open(filename, 'w') as json_file:
-            json.dump(all_employee_tasks, json_file)
-
-        print(f"Data exported to {filename}")
-
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    filename = "todo_all_employees.json"
+    with open(filename, 'w') as json_file:
+        json.dump(all_employee_tasks, json_file)
