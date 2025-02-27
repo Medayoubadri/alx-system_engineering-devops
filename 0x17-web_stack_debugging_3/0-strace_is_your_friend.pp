05c2348815034a0c516c973dd6a9519ebb2e1b76
medayoubadri <medayoubadri@gmail.com>
2025-02-27 20:55:17 +0000

    All tasks finished

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes the Apache 500 error by correcting file extension in wp-settings.php
-exec { 'fix wordpress':
+exec { 'fix-wordpress':
   command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
