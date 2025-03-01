d40e1668672671c7ee7878a0b8c7e1d57843ee99
medayoubadri <medayoubadri@gmail.com>
2025-03-01 15:32:53 +0000

    discord

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,15 +4,15 @@ exec { 'fix-wordpress':
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Send debug information to webhook
+# Send debug information to Discord webhook
 exec { 'send-debug-info':
   command => 'BEFORE=$(grep -c "phpp" /var/www/html/wp-settings.php || echo "0"); \
              sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; \
              AFTER=$(grep -c "php" /var/www/html/wp-settings.php || echo "0"); \
              HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" 127.0.0.1); \
              curl -X POST -H "Content-Type: application/json" \
-             -d "{\\"before\\":\\"$BEFORE\\",\\"after\\":\\"$AFTER\\",\\"status\\":\\"$HTTP_STATUS\\"}" \
-             https://webhook.site/b0d8d6b9-7f81-4eb0-a2e9-cd8ec92307a2',
+             -d "{\\"content\\":\\"Debug Info:\\nBefore: $BEFORE\\nAfter: $AFTER\\nStatus: $HTTP_STATUS\\"}" \
+             https://discord.com/api/webhooks/1345418223121600612/pUvQKi37llCljekjd5qzCVQh-1Br0mzfAfbN9bwdlOOjEkrM-f6Pz5Sb41_m-ibpZybb',
   path    => '/usr/local/bin/:/bin/',
   require => Exec['fix-wordpress']
 }
