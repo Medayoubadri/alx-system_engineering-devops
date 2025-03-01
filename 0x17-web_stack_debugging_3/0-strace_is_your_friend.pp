28f09364f44cb781742fa6cf7e46596be1279df7
medayoubadri <medayoubadri@gmail.com>
2025-03-01 13:12:23 +0000

    html

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,25 @@
-# Fixes the Apache 500 error by correcting file extension in wp-settings.php
-
-exec { 'Correct typo in file extension':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell
+# Fixes Apache 500 error and ensures WordPress displays correctly
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
+# Ensure WordPress database has the correct site title and tagline
+exec { 'update-wp-config':
+  command     => 'mysql -u root wordpress -e "UPDATE wp_options SET option_value=\'ALX\' \
+                  WHERE option_name=\'blogname\'; \
+                  UPDATE wp_options SET option_value=\'Yet another bug by a ALX student\' \
+                  WHERE option_name=\'blogdescription\';"',
+  path        => '/usr/bin/:/bin/',
+  refreshonly => true,
+  subscribe   => Exec['fix-wordpress'],
+}
+
+# Ensure proper permissions on WordPress files
+file { '/var/www/html':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  recurse => true,
+  require => Exec['fix-wordpress'],
+}
